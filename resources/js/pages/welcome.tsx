400dcc48a9b1a09b3582ac404db6210ae9f4b081
Sarwar Ahmed <sarwar.amas@gmail.com>
2025-09-05 14:35:01 +0600

    ToggleDropdown for Theme switching

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,3 +1,4 @@
+import AppearanceToggleDropdown from '@/components/appearance-dropdown';
 import { NavigationMenuHome } from '@/pages/amas/navigation-menu';
 import { type SharedData } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
@@ -12,8 +13,14 @@ export default function Welcome() {
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
 
-            <div className="flex h-full w-full flex-col items-center justify-center bg-background text-foreground">
-                <NavigationMenuHome />
+            <div className="container mx-auto flex h-full w-full flex-col items-center justify-center bg-background text-foreground">
+                <div className="flex w-full items-center justify-between gap-4 px-4 py-2">
+                    <div className="flex flex-col items-center justify-center gap-4">Home</div>
+                    <NavigationMenuHome />
+                    <div className="flex flex-col items-center justify-center gap-4">
+                        <AppearanceToggleDropdown />
+                    </div>
+                </div>
             </div>
         </>
     );
