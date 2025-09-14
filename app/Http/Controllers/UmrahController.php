965696a6ba994bffcf285cbb5ebeeb6bbc358cec
Sarwar Ahmed <sarwar.amas@gmail.com>
2025-09-14 09:17:36 +0600

    WIP: umrah form

--- app/Http/Controllers/UmrahController.php
+++ app/Http/Controllers/UmrahController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UmrahPackageRequest;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class UmrahController extends Controller
@@ -9,5 +11,11 @@ class UmrahController extends Controller
     public function create()
     {
         return Inertia::render('umrah/create');
+    }
+
+    public function store(UmrahPackageRequest $request)
+    {
+        $umrah = $request->validated();
+        dd($umrah);
     }
 }
