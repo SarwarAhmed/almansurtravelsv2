e729cf74c76b3633ad0b127e55ddb13a3e8047c7
Sarwar Ahmed <sarwar.amas@gmail.com>
2025-10-27 23:58:21 +0600

    update: service UI

--- resources/js/components/OurServices.tsx
+++ resources/js/components/OurServices.tsx
@@ -47,33 +47,30 @@ const services = [
 ];
 export default function OurServices() {
     return (
-        <div className="container mx-auto max-w-7xl px-4">
+        <div className="xpx-4 container mx-auto max-w-7xl">
             <div className="xmax-w-3xl container mx-auto mt-3 w-full rounded-2xl">
-                <div className="relative bg-gray-50 px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
-                    <div className="absolute inset-0">
-                        <div className="h-1/3 bg-white sm:h-2/3" />
-                    </div>
+                <div className="relative px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28 dark:bg-gray-900">
                     <div className="relative mx-auto max-w-7xl">
                         <div className="text-center">
-                            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Our Services</h2>
+                            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-300">Our Services</h2>
                             <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
                                 Comprehensive travel solutions tailored to your needs
                             </p>
                         </div>
-                        <div className="mx-auto mt-12 grid max-w-lg gap-5 overflow-hidden lg:max-w-none lg:grid-cols-3">
+                        <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
                             {services.map((service) => (
-                                <div key={service.title} className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                                <div key={service.title} className="flex flex-col overflow-hidden rounded-lg shadow-lg dark:shadow-gray-800">
                                     <div className="flex-shrink-0">
                                         <img
                                             className="relative h-48 w-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
                                             src={service.imageUrl}
                                             alt=""
                                         />
-                                        <div className="absolute -mt-2 mb-2 items-center justify-center rounded-r-xl bg-gray-900 p-2 font-bold text-white">
+                                        <div className="absolute -mt-2 mb-2 items-center justify-center rounded-r-xl bg-gray-800 p-2 font-bold text-white dark:bg-gray-950">
                                             {service.name}
                                         </div>
                                     </div>
-                                    <div className="mt-5 flex flex-1 flex-col justify-between bg-white p-6">
+                                    <div className="mt-5 flex flex-1 flex-col justify-between bg-white p-6 dark:bg-gray-900">
                                         <div className="flex-1">
                                             <p className="text-sm font-medium text-indigo-600">
                                                 <a href={service.category.href} className="hover:underline">
@@ -81,7 +78,9 @@ export default function OurServices() {
                                                 </a>
                                             </p>
                                             <a href={service.href} className="mt-2 block">
-                                                <p className="text-xl font-semibold text-gray-900">{service.title}</p>
+                                                <p className="text-xl font-semibold text-gray-900 hover:underline dark:text-gray-200">
+                                                    {service.title}
+                                                </p>
                                             </a>
                                         </div>
                                     </div>
